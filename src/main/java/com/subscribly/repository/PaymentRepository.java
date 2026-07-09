f1353e1e85f87a14b924f603b11fe6eb96cdbbf4
himanshukumar8 <69477742+himanshukumar8@users.noreply.github.com>
2026-07-09 07:08:21 +0530

    Add method to find payments to process with query

--- src/main/java/com/subscribly/repository/PaymentRepository.java
+++ src/main/java/com/subscribly/repository/PaymentRepository.java
@@ -2,16 +2,26 @@ package com.subscribly.repository;
 
 import com.subscribly.entity.Payment;
 import com.subscribly.entity.PaymentStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+   
     List<Payment> findBySubscriptionUserEmail(String email);
-
     List<Payment> findByStatus(PaymentStatus status);
 
-    List<Payment> findByStatusAndNextRetryAtBefore(PaymentStatus status, LocalDate date);
+    
+    @Query("SELECT p FROM Payment p WHERE (p.status = :retryingStatus AND p.nextRetryAt <= :now) OR p.status = :pendingStatus")
+    List<Payment> findPaymentsToProcess(
+        @Param("retryingStatus") PaymentStatus retryingStatus,
+        @Param("pendingStatus") PaymentStatus pendingStatus,
+        @Param("now") LocalDateTime now,
+        Pageable pageable
+    );
 }
